1af3c0f0afbb9fa3842ed1b6e765e9a0a4da6374
karthikeyanjanakiraman-cell <karthikeyan.janakiraman@gmail.com>
2026-07-10 18:17:47 +0530

    Update LOWVOL.py

--- LOWVOL.py
+++ LOWVOL.py
@@ -443,11 +443,6 @@ def main():
     market_open = now.replace(hour=9, minute=15, second=0, microsecond=0)
     market_close = now.replace(hour=15, minute=30, second=0, microsecond=0)
     
-    # Cron Gate: Prevent execution outside market hours
-    if now < market_open or now > market_close:
-        logger.info("Outside market hours. Scanner sleeping.")
-        return
-
     fyers = init_fyers()
     if not fyers: return
 
