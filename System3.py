ed1835e4f4dc9ad274f0ca95fbcae08f8f3444ec
karthikeyanjanakiraman-cell <karthikeyan.janakiraman@gmail.com>
2026-08-16 16:46:12 +0530

    Update System3.py

--- System3.py
+++ System3.py
@@ -42,7 +42,7 @@ COLOR_DIM = "\033[2m"
 COLOR_RESET = "\033[0m"
 COLOR_BOLD = "\033[1m"
 
-BACKTRACE_DAYS = 20
+BACKTRACE_DAYS = 1
 MAX_BREACH_DAYS = 0
 
 API_ERROR_LOGGED = False
@@ -70,8 +70,8 @@ ADX_THRESHOLD = 20
 STOCH_PERIOD = 14
 
 # 3. 45-Degree Renko Parameters (Dual-Tier Calibration)
-MICRO_RENKO_CONFIRM_BRICKS = 2  # Micro Tactical Trigger (2-Brick Rule)
-MACRO_RENKO_CONFIRM_BRICKS = 1  # Macro Structural Trend Confirmation
+MICRO_RENKO_CONFIRM_BRICKS = 1  # Micro Tactical Trigger (2-Brick Rule)
+MACRO_RENKO_CONFIRM_BRICKS = 0  # Macro Structural Trend Confirmation
 RENKO_MIN_BRICK = 0.05
 RENKO_DEFAULT_PCT = 0.005
 
