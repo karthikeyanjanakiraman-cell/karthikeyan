efea54004c81ddfc34efba0cec59acc6b959ae42
karthikeyanjanakiraman-cell <karthikeyan.janakiraman@gmail.com>
2026-08-17 00:14:57 +0530

    Update System3.py

--- System3.py
+++ System3.py
@@ -47,8 +47,8 @@ API_ERROR_LOGGED = False
 # ==============================================================================
 # ★ GLOBAL CONFIGURATION: DYNAMIC TIMEFRAMES & INDICATORS ★
 # ==============================================================================
-MICRO_TIMEFRAME = "1min"  # Micro Execution & Tactical Trigger
-MACRO_TIMEFRAMES = ["5min","10min", "15min", "30min", "60min", "240min", "1D"]  # Macro Structural Tiers
+MICRO_TIMEFRAME = "1D"  # Micro Execution & Tactical Trigger
+MACRO_TIMEFRAMES ["5D"]  # Macro Structural Tiers
 
 ATR_PERIOD = 14
 RSI_PERIOD = 14
