d731ce81dc346523b5c38fba36b3c4effdce7cdf
karthikeyanjanakiraman-cell <karthikeyan.janakiraman@gmail.com>
2026-08-20 12:04:19 +0530

    Update System5.py

--- System5.py
+++ System5.py
@@ -43,13 +43,13 @@ COLOR_BOLD = "\033[1m"
 # ★ GLOBAL CONFIGURATION: OPTIONS, TIMEFRAMES & INDICATORS ★
 # ==============================================================================
 # Options Selection Configurations
-STRIKE_RANGE_OFFSET = 2      # Number of strikes above and below ATM
+STRIKE_RANGE_OFFSET = 1      # Number of strikes above and below ATM
 TARGET_EXPIRY = "CURRENT"    # "CURRENT" or "NEXT"
 BACKTRACE_DAYS = 7          # Reduced to manage data load
 
 # 🌟 NEW PRE-FILTER VARIABLES (The Speed Enablers)
-MIN_OPT_PREMIUM = 10.0        # Option must close >= ₹5.00 on the previous day
-MIN_PREV_DAY_VOLUME = 100000  # Option must have traded >= 10,000 contracts on the previous day
+MIN_OPT_PREMIUM = 30.0        # Option must close >= ₹5.00 on the previous day
+MIN_PREV_DAY_VOLUME = 500000  # Option must have traded >= 10,000 contracts on the previous day
 
 MAX_API_WORKERS = 40         # Aggressive threading worker count
 
