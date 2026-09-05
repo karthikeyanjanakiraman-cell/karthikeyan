858c4356ffda5d9e13fb73bd308690b48ee6915e
karthikeyanjanakiraman-cell <karthikeyan.janakiraman@gmail.com>
2026-09-05 19:01:55 +0530

    Update System3.py

--- System3.py
+++ System3.py
@@ -82,7 +82,7 @@ def _log_fyers_error(context, status_code=None, body=None):
 # 🎛️ TIER 0: TRADING MODE, PIPELINE ROUTING & DATA FEED SWITCH
 # ==============================================================================
 DATA_FEED_MODE = "REST"           
-TRADING_MODE = "CASH_EQUITY"       
+TRADING_MODE = "CAASH_EQUITY"       
 ENABLE_STAGE1_STOCK_FILTER = False  
 
 MIN_STOCK_PRICE = 100.0
