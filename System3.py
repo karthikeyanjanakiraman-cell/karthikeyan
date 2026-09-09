9888bb8ab444c46de3564b670c1bc5f27bdaffcd
karthikeyanjanakiraman-cell <karthikeyan.janakiraman@gmail.com>
2026-09-09 14:03:00 +0530

    Update System3.py

--- System3.py
+++ System3.py
@@ -94,7 +94,7 @@ MIN_STOCK_VOLUME = 500000
 # GLOBAL CONFIGURATION
 # ==============================================================================
 MICRO_TIMEFRAME = "5min"
-MACRO_TIMEFRAMES = ["45min"]
+MACRO_TIMEFRAMES = ["60min"]
 
 ATR_PERIOD = 14
 RSI_PERIOD = 14
